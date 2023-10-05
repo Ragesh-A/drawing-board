46d583a0b9ce0ab05023706f0ce49f004c384ed6
Ragesh-A <ragesha2017@gmail.com>
2023-10-05 17:00:21 +0530

    refactor

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,60 +1,57 @@
-import { useContext, createElement } from 'react';
+import { useContext } from 'react';
 import { Context } from '../../utils/context/context';
 import Palate from '../palate/Palate';
 import './style.css';
 
 const SideBar = () => {
-  const { color, changeColor, changeBrushSize, canvasContext, canvasRef } = useContext(Context);
+	const { color, changeColor, changeBrushSize, canvasContext, canvasRef } =
+		useContext(Context);
 
-  const clearCanvas = () => {
-    if (canvasContext) {
-      console.log(canvasContext.clearRect(0, 0, window.innerWidth, window.innerHeight));
-    }
-  }
+	const clearCanvas = () => {
+		if (canvasContext) {
+			canvasContext.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		}
+	};
 
-  const downloadImage = () => {
-    const imageUrl = canvasRef.current.toDataURL('image/png');
-    const link = document.createElement('a')
-    link.href = imageUrl;
-    link.download = 'drawing.png';
-    link.click()
-  }
+	const downloadImage = () => {
+		const imageUrl = canvasRef.current.toDataURL('image/png');
+		const link = document.createElement('a');
+		link.href = imageUrl;
+		link.download = 'drawing.png';
+		link.click();
+	};
 
-  return (
-    <div id="sidebar">
+	return (
+		<div id='sidebar'>
+			<Palate />
+			<input
+				type='color'
+				className='palate'
+				value={color}
+				onChange={(e) => changeColor(e.target.value)}
+			/>
 
-      <Palate />
-      <input
-        type="color"
-        className="palate"
-        value={color}
-        onChange={(e) => changeColor(e.target.value)}
-      />
+			<button onClick={(e) => changeColor('#272727')} className='mt-2 btn'>
+				<img src='/icons/eraser.svg' className='icon' alt='eraser button' />
+			</button>
 
-      <button onClick={(e) => changeColor('#272727')} className="mt-2" >
-        erase
-      </button>
+			<button className='btn' onClick={clearCanvas}>
+				<img src='/icons/clear.svg' className='icon' alt='clear canvas button' />
+			</button>
+			<button className='btn' onClick={downloadImage}>
+				<img src='/icons/download.svg' className='icon' alt='download drawing button' />
+			</button>
 
-      <button className='btn' onClick={clearCanvas}>
-        clear
-      </button>
-
-      <button className='btn' onClick={downloadImage}>
-        download
-      </button>
-      
-      <input
-        type="number"
-        name=""
-        id=""
-        min={1}
-        max={30}
-        defaultValue={2}
-        onChange={(e) => changeBrushSize(e.target.value)}
-        className="mt-2"
-      />
-    </div>
-  );
+			<input
+				type='number'
+				min={1}
+				max={30}
+				defaultValue={2}
+				onChange={(e) => changeBrushSize(e.target.value)}
+				className='mt-2 btn'
+			/>
+		</div>
+	);
 };
 
 export default SideBar;
